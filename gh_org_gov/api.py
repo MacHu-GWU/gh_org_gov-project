381ac66b0388f6869e81aa71e5e5ff4a1c4ad516
Sanhe Hu <6800411+MacHu-GWU@users.noreply.github.com>
2026-04-15 00:28:21 -0400

    Expose public symbols from gh_org_gov.api
    Re-export key public types and functions from submodules (constants, client, team_def, team, repo) in gh_org_gov/api.py so they are available from the package top-level API. Update tests/test_api.py to assert the presence of these exported symbols, ensuring a single import surface for package consumers.

--- gh_org_gov/api.py
+++ gh_org_gov/api.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from .constants.constants import RepositoryRoleEnum
+from .constants.constants import TeamDefTsvColEnum
+from .client import get_github
+from .client import graphql_request
+from .team_def import TeamPrivacyEnum
+from .team_def import TeamNotificationSettingEnum
+from .team_def import TeamDef
+from .team_def import ExistingTeamDef
+from .team_def import load_team_defs_from_tsv
+from .team import TeamSyncResult
+from .team import plan_sync
+from .team import fetch_existing_team_defs
+from .team import sync_teams
+from .repo import RepoPermChange
+from .repo import RepoPermSyncResult
+from .repo import fetch_repo_team_permissions
+from .repo import plan_sync_repo_permissions
+from .repo import sync_repo_permissions
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,24 @@ from gh_org_gov import api
 
 def test():
     _ = api
+    _ = api.RepositoryRoleEnum
+    _ = api.TeamDefTsvColEnum
+    _ = api.get_github
+    _ = api.graphql_request
+    _ = api.TeamPrivacyEnum
+    _ = api.TeamNotificationSettingEnum
+    _ = api.TeamDef
+    _ = api.ExistingTeamDef
+    _ = api.load_team_defs_from_tsv
+    _ = api.TeamSyncResult
+    _ = api.plan_sync
+    _ = api.fetch_existing_team_defs
+    _ = api.sync_teams
+    _ = api.RepoPermChange
+    _ = api.RepoPermSyncResult
+    _ = api.fetch_repo_team_permissions
+    _ = api.plan_sync_repo_permissions
+    _ = api.sync_repo_permissions
 
 
 if __name__ == "__main__":
